9cd641dce8c2c7cf28ae7816e6f00ddf782697fd
BojunLi03 <bojun.li.03@gmail.com>
2024-11-11 18:14:06 -0500

    Initial Commit

---
+++ SimpleContacts/SimpleContactsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleContactsApp.swift
+//  SimpleContacts
+//
+//  Created by Bojun Li on 11/11/24.
+//
+
+import SwiftUI
+
+@main
+struct SimpleContactsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
